8dacb4eea12701ab555be6af25fa721d28dc4df3
🔹Esteban <esteban.horacio.acevedo@gmail.com>
2025-08-07 19:46:51 -0300

    Agrega archivo sobre números enteros

---
+++ 1_enteros.py
@@ -0,0 +1,9 @@
+# Enteros
+
+entero1 = 0
+entero2 = 10
+entero3 = -10
+entero4 = 1_000_000
+
+print(type(entero1), type(entero2), type(entero3), type(entero4))
+print(entero1, entero2, entero3, entero4)
